7addb6aca133cadbdd2f2a0ca697e354b89018b1
chandoiwe <lntrung1989@yahoo.com>
2016-06-06 22:27:04 +0700

    Commit createTest

--- SRC/LearnLanguageAPI/src/services/ExerciseService.java
+++ SRC/LearnLanguageAPI/src/services/ExerciseService.java
@@ -6,13 +6,13 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
-import util.StringUtil;
 import dao.CommonDAO;
 import dto.ExerciseDto;
 import enums.QuestionType;
+import util.StringUtil;
 
 public class ExerciseService {
-	 
+
 	/**
 	 * Search Exercise
 	 *
@@ -54,7 +54,7 @@ public class ExerciseService {
 			sqlQuery.append("   AND T1.LESSON_ID = T2.LESSON_ID \n");
 			sqlQuery.append("   AND T1.LESSON_ID = ? \n");
 			sqlQuery.append(" ORDER BY \n");
-			sqlQuery.append("   T2.EXERCISE_ID \n");
+			sqlQuery.append("   RAND() \n");
 
 			// Create Statement
 			stmt = conn.prepareStatement(sqlQuery.toString());
@@ -107,5 +107,207 @@ public class ExerciseService {
 		}
 
 		return exerciseDtoList;
+	}
+
+	/**
+	 * Create test
+	 *
+	 * @param lessonId
+	 * @param vocabularyNumber
+	 * @param grammarNumber
+	 * @param readingNumber
+	 * @param listeningNumber
+	 * @param conversationNumber
+	 * @param kanjiNumber
+	 * @return List<ExerciseDto>
+	 */
+	public static List<ExerciseDto> createTest(
+			Long lessonId, Long vocabularyNumber, Long grammarNumber,
+			Long readingNumber, Long listeningNumber, Long conversationNumber,
+			Long kanjiNumber) {
+
+		// Result list
+		List<ExerciseDto> exerciseDtoList = new ArrayList<ExerciseDto>();
+
+		// Get DAO
+		Connection conn = CommonDAO.getDAO();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			// Create SQL
+			StringBuffer sqlQuery = new StringBuffer();
+
+			// vocabularyNumber
+			if (vocabularyNumber != null && vocabularyNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.VOCABULARY));
+			}
+			// grammarNumber
+			if (grammarNumber != null && grammarNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.GRAMMAR));
+			}
+			// readingNumber
+			if (readingNumber != null && readingNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.READING));
+			}
+			// listeningNumber
+			if (listeningNumber != null && listeningNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.LISTENING));
+			}
+			// conversationNumber
+			if (conversationNumber != null && conversationNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.CONVERSATION));
+			}
+			// kanjiNumber
+			if (kanjiNumber != null && kanjiNumber > 0) {
+				sqlQuery.append(createTest_CreateSQL(QuestionType.KANJI));
+			}
+			// Default
+			sqlQuery.append(createTest_CreateSQL(null));
+
+			// Create Statement
+			stmt = conn.prepareStatement(sqlQuery.toString());
+
+			// Add condition
+			int conditionIndex = 0;
+
+			// vocabularyNumber
+			if (vocabularyNumber != null && vocabularyNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.VOCABULARY, vocabularyNumber);
+				conditionIndex += 3;
+			}
+			// grammarNumber
+			if (grammarNumber != null && grammarNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.GRAMMAR, grammarNumber);
+				conditionIndex += 3;
+			}
+			// readingNumber
+			if (readingNumber != null && readingNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.READING, readingNumber);
+				conditionIndex += 3;
+			}
+			// listeningNumber
+			if (listeningNumber != null && listeningNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.LISTENING, listeningNumber);
+				conditionIndex += 3;
+			}
+			// conversationNumber
+			if (conversationNumber != null && conversationNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.CONVERSATION, conversationNumber);
+				conditionIndex += 3;
+			}
+			// kanjiNumber
+			if (kanjiNumber != null && kanjiNumber > 0) {
+				createTest_AddCondition(stmt, conditionIndex, lessonId,
+										QuestionType.KANJI, kanjiNumber);
+				conditionIndex += 3;
+			}
+			// Default
+			stmt.setLong(conditionIndex + 1, lessonId);
+
+			// Execute query
+			rs = stmt.executeQuery();
+			while (rs.next()) {
+				// Edit Dto
+				ExerciseDto exerciseDto = new ExerciseDto();
+				exerciseDto.setExerciseId(rs.getLong("EXERCISE_ID"));
+				exerciseDto.setLessonId(rs.getLong("LESSON_ID"));
+				exerciseDto.setQuestionType(rs.getString("QUESTION_TYPE"));
+				exerciseDto.setAnswerType(rs.getString("ANSWER_TYPE"));
+				exerciseDto.setQuestionContent(rs.getString("QUESTION_CONTENT"));
+				exerciseDto.setQuestionContentFile(rs.getString("QUESTION_CONTENT_FILE"));
+				exerciseDto.setMark(rs.getInt("MARK"));
+				exerciseDto.setTime(rs.getInt("TIME"));
+				exerciseDto.setAnswerChoose(rs.getString("ANSWER_CHOOSE"));
+				exerciseDto.setAnswer1(rs.getString("ANSWER_1"));
+				exerciseDto.setAnswer2(rs.getString("ANSWER_2"));
+				exerciseDto.setAnswer3(rs.getString("ANSWER_3"));
+				exerciseDto.setAnswer4(rs.getString("ANSWER_4"));
+				exerciseDto.setAnswer5(rs.getString("ANSWER_5"));
+
+				// Get File
+				if (!StringUtil.isNullOrEmpty(exerciseDto.getQuestionContentFile())) {
+					exerciseDto.setQuestionContentFileStream(
+						StringUtil.readUploadFile(exerciseDto.getQuestionContentFile()));
+				}
+
+				// Add Dto to List
+				exerciseDtoList.add(exerciseDto);
+			}
+
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+			} catch (Exception ex) {
+			}
+		}
+
+		return exerciseDtoList;
+	}
+
+	private static String createTest_CreateSQL(QuestionType questionType) {
+		StringBuffer sqlQuery = new StringBuffer();
+		sqlQuery.append(" SELECT \n");
+		sqlQuery.append("   T_EXERCISE.EXERCISE_ID \n");
+		sqlQuery.append("  ,T_EXERCISE.LESSON_ID \n");
+		sqlQuery.append("  ,T_EXERCISE.QUESTION_TYPE \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_TYPE \n");
+		sqlQuery.append("  ,T_EXERCISE.QUESTION_CONTENT \n");
+		sqlQuery.append("  ,T_EXERCISE.QUESTION_CONTENT_FILE \n");
+		sqlQuery.append("  ,T_EXERCISE.MARK \n");
+		sqlQuery.append("  ,T_EXERCISE.TIME \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_CHOOSE \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_1 \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_2 \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_3 \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_4 \n");
+		sqlQuery.append("  ,T_EXERCISE.ANSWER_5 \n");
+		sqlQuery.append(" FROM \n");
+		sqlQuery.append("   T_LESSON \n");
+		sqlQuery.append("  ,T_EXERCISE \n");
+		sqlQuery.append(" WHERE \n");
+		sqlQuery.append("       1 = 1 \n");
+		sqlQuery.append("   AND T_LESSON.LESSON_ID = T_EXERCISE.LESSON_ID \n");
+		sqlQuery.append("   AND T_LESSON.LESSON_NO <= \n");
+		sqlQuery.append("                            ( \n");
+		sqlQuery.append("                             SELECT \n");
+		sqlQuery.append("                               ST.LESSON_NO \n");
+		sqlQuery.append("                             FROM \n");
+		sqlQuery.append("                               T_LESSON ST \n");
+		sqlQuery.append("                             WHERE \n");
+		sqlQuery.append("                               ST.LESSON_ID = ? \n");
+		sqlQuery.append("                            ) \n");
+		if (questionType != null) {
+			sqlQuery.append("   AND T_EXERCISE.QUESTION_TYPE = ? \n");
+			sqlQuery.append(" ORDER BY RAND() \n");
+			sqlQuery.append(" LIMIT ? \n");
+			sqlQuery.append(" UNION \n");
+		} else {
+			sqlQuery.append(" ORDER BY RAND() \n");
+			sqlQuery.append(" LIMIT 50 \n");
+		}
+
+		return sqlQuery.toString();
+	}
+
+	private static void createTest_AddCondition(
+			PreparedStatement stmt, int conditionIndex, Long lessonId,
+			QuestionType questionType, Long number) throws Exception {
+
+		stmt.setLong(conditionIndex + 1, lessonId);
+		stmt.setString(conditionIndex + 2, questionType.getCode());
+		stmt.setLong(conditionIndex + 3, number);
+
 	}
 }
--- SRC/LearnLanguageAPI/src/webservices/ExerciseAPI.java
+++ SRC/LearnLanguageAPI/src/webservices/ExerciseAPI.java
@@ -52,4 +52,66 @@ public class ExerciseAPI {
 		return result.toString();
 	}
 
+	@POST
+	@Path("/createTest.json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String createTest(String requestBody) {
+		// Create JsonObject to send back
+		JsonParser jsonParser = new JsonParser();
+		JsonObject result = new JsonObject();
+
+		try {
+			// Get parameter
+			JsonObject body = jsonParser.parse(requestBody).getAsJsonObject();
+			if (!body.has("lessonId")) {
+				// If parameter is not exist, error 001
+				result.addProperty("ERR001", Constants.ERR_001);
+
+			} else {
+				// Get LessonId
+				Long lessonId = body.get("lessonId").getAsLong();
+				Long vocabularyNumber = null;
+				if (body.has("vocabularyNumber")) {
+					vocabularyNumber = body.get("vocabularyNumber").getAsLong();
+				}
+				Long grammarNumber = null;
+				if (body.has("grammarNumber")) {
+					grammarNumber = body.get("grammarNumber").getAsLong();
+				}
+				Long readingNumber = null;
+				if (body.has("readingNumber")) {
+					readingNumber = body.get("readingNumber").getAsLong();
+				}
+				Long listeningNumber = null;
+				if (body.has("listeningNumber")) {
+					listeningNumber = body.get("listeningNumber").getAsLong();
+				}
+				Long conversationNumber = null;
+				if (body.has("conversationNumber")) {
+					conversationNumber = body.get("conversationNumber").getAsLong();
+				}
+				Long kanjiNumber = null;
+				if (body.has("kanjiNumber")) {
+					kanjiNumber = body.get("kanjiNumber").getAsLong();
+				}
+
+				// Search Exercise
+				List<ExerciseDto> exerciseDtoList =
+					ExerciseService.createTest(
+						lessonId, vocabularyNumber, grammarNumber,
+						readingNumber, listeningNumber, conversationNumber,
+						kanjiNumber);
+
+				Gson gson = new Gson();
+				String jsonData = gson.toJson(exerciseDtoList);
+				result.add("data", jsonParser.parse(jsonData));
+			}
+
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			result.addProperty("ERR999", Constants.ERR_999);
+		}
+
+		return result.toString();
+	}
 }
